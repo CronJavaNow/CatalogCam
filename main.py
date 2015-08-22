47ca921aad87e2ca462afb0b5edb1b8891e1b5c7
justin <justinrgarr@gmail.com>
2015-08-22 14:05:27 -0400

    doozy day!

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import pygame
 import pygame.camera
-from collections import Counter
 
 pygame.init()
 pygame.camera.init()
-cnt = Counter()
 
-camlst = pygame.camera.list_cameras()
+camlst = dict(enumerate(pygame.camera.list_cameras()))
 
 pickcam = input("Would you like a list connected cameras? \n please type:|Yes or press Enter| ")
 
 def user_camsnap():
-    print("Something cool will take place soon!")
     print(camlst)
+    usercam = int(input("Please enter the id/number of the camera you would like to use: "))
+    cam = pygame.camera.Camera(camlst[usercam], (352,288))
+    cam.start()
+    image= cam.get_image()
+    pygame.image.save(image,'images/101.jpg')
+    cam.stop()
 
 def default_camsnap():
     cam = pygame.camera.Camera(camlst[0], (352,288))
---
+++ test.py
@@ -0,0 +1,6 @@
+
+testlst = ('string1', "string2", "string3", "string4", "string5")
+testd = dict(enumerate(testlst))
+
+print(testd)
+
